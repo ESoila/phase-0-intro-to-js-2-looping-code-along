a975c06c777d4746e1a07eba95e640504956c76e
soila <elizabeth.soila@student.moringaschool.com>
2024-12-12 22:21:40 +0300

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+  writeCards(cards);
+
+  function writeCards(names, event) {
+    const messages = []; 
+    for (let i = 0; i < names.length; i++) { 
+
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages; 
+  }
+  
+
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+  let countdown = 10;
+while (countdown > 0) {
+  console.log(countdown);
+  countdown--;
+}
+ 
+function countDown(number) {
+    while (number >= 0) { 
+      console.log(number); 
+      number--; 
+    }
+  }
+  
+  
